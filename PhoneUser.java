aac5fe2f2adc96d055961398b9581f4e27668206
Nathanflx <nathanaelfh78@gmail.com>
2024-02-01 10:18:35 +0700

    first commit

---
+++ PhoneUser.java
@@ -0,0 +1,31 @@
+package contohinterface;
+
+
+import contohinterface.Phone;
+
+public class PhoneUser {
+    public class phoneuser {
+    
+    private Phone phone;
+
+    public phoneuser(Phone phone) {
+        this.phone = phone;
+    }
+    
+    void turnOnThePhone(){
+        this.phone.powerOn();
+    }
+    
+    void turnOffThePhone(){
+        this.phone.powerOff();
+    }
+    
+    void makePhoneLouder(){
+        this.phone.volumeUp();
+    }
+    
+    void makePhoneSilent(){
+        this.phone.volumeDown();
+    }
+}
+}
